4bcaf18a89e3aaac75dc61a98a0a0838d681414e
SEOYOUNGJIN <youngjin.seo@e4net.net>
2026-01-16 20:22:49 +0900

    Header 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,11 @@ export default function Header() {
     <Link to="/">
       <div className="retreat-header">
         <div className="overlay">
-          <h1 className="title">2026 청년부 겨울수련회</h1>
-          <h2 className="subtitle">청년이여 지금 기도하라!</h2>
+          <h2 className="title">2026 청년부 겨울수련회</h2>
+          <h3 className="subtitle">청년이여 지금 기도하라!</h3>
           <p className="verse">
-            “새벽 아직도 밝기 전에 예수께서 일어나 나가 한적한 곳으로
-            <br /> 가사거기서 기도하시더니”
+            “새벽 아직도 밝기 전에 예수께서 일어나
+            <br /> 나가 한적한 곳으로 가사거기서 기도하시더니”
             <br /> <span className="reference">– 마가복음 1:35</span>
           </p>
         </div>
